e1a7bf3c95250270ec6db451dea0238a5193f5f7
Martina <apostoloska21@gmail.com>
2024-10-12 21:55:41 +0200

    fixing up font color when in light mode

--- app/components/NavLink.jsx
+++ app/components/NavLink.jsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 const NavLink = ({ href, title }) => {
   const isExternal = href.startsWith('http');
   
+  const linkClasses = "block py-2 pl-3 pr-4 text-gray-800 dark:text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-black dark:hover:text-white";
+  
   if (isExternal) {
     return (
       <a
         href={href}
-        className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
+        className={linkClasses}
         target="_blank"
         rel="noopener noreferrer"
       >
@@ -17,10 +19,7 @@ const NavLink = ({ href, title }) => {
   }
 
   return (
-    <Link
-      href={href}
-      className="block py-2 pl-3 pr-4 text-[#ADB7BE] sm:text-xl rounded md:p-0 hover:text-white"
-    >
+    <Link href={href} className={linkClasses}>
       {title}
     </Link>
   );
--- app/components/TabButton.jsx
+++ app/components/TabButton.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export const TabButton = ({ active, selectTab, children }) => {
   const buttonClasses = active
-    ? "text-white border-b border-purple-500"
-    : "text-[#ADB7BE]"; 
+    ? "text-gray-800 dark:text-white border-b border-purple-500"
+    : "text-gray-500 dark:text-[#ADB7BE]"; 
 
   return (
     <button onClick={selectTab}>
-      <p className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}> 
+      <p className={`mr-3 font-semibold hover:text-gray-800 dark:hover:text-white ${buttonClasses}`}> 
         {children}
       </p>
     </button>
