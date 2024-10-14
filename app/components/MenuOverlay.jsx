5e1069c93dffb9e9bf719adc3b5520563be0c591
Martina <apostoloska21@gmail.com>
2024-10-14 13:00:19 +0200

    improving responsive design: adding switch button to be visible on mobile

--- app/components/MenuOverlay.jsx
+++ app/components/MenuOverlay.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import NavLink from "./NavLink";
+import ThemeButton from './ThemeButton';
+
 const MenuOverlay = ({ links }) => {
   return (
     <ul className="flex flex-col py-4 items-center backdrop-blur-sm bg-white/70 dark:bg-gray-800/70 rounded-lg shadow-lg transition-all duration-300">
@@ -8,7 +10,11 @@ const MenuOverlay = ({ links }) => {
           <NavLink href={link.path} title={link.title} />
         </li>
       ))}
+      <li className="mt-4">
+        <ThemeButton />
+      </li>
     </ul>
   );
 };
+
 export default MenuOverlay;
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -32,34 +32,34 @@ const Navbar = () => {
         >
           MABYSS
         </Link>
-        <div className="mobile-menu block md:hidden">
-          {!navbarOpen ? (
-            <button
-              onClick={() => setNavbarOpen(true)}
-              className="flex items-center px-3 py-2 border rounded border-gray-800 dark:border-white text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
-            >
-              <Bars3Icon className="h-5 w-5" />
-            </button>
-          ) : (
-            <button
-              onClick={() => setNavbarOpen(false)}
-              className="flex items-center px-3 py-2 border rounded border-gray-800 dark:border-white text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
-            >
-              <XMarkIcon className="h-5 w-5" />
-            </button>
-          )}
-        </div>
         <div className="flex items-center">
-          <div className="menu hidden md:block md:w-auto ml-4" id="navbar">
-            <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
-              {navLinks.map((link, index) => (
-                <li key={index}>
-                  <NavLink href={link.path} title={link.title} />
-                </li>
-              ))}
-              <li> <ThemeButton /></li>
-            </ul>
+          <ThemeButton />
+          <div className="mobile-menu block md:hidden ml-4">
+            {!navbarOpen ? (
+              <button
+                onClick={() => setNavbarOpen(true)}
+                className="flex items-center px-3 py-2 border rounded border-gray-800 dark:border-white text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
+              >
+                <Bars3Icon className="h-5 w-5" />
+              </button>
+            ) : (
+              <button
+                onClick={() => setNavbarOpen(false)}
+                className="flex items-center px-3 py-2 border rounded border-gray-800 dark:border-white text-gray-800 dark:text-white hover:text-gray-600 dark:hover:text-gray-400"
+              >
+                <XMarkIcon className="h-5 w-5" />
+              </button>
+            )}
           </div>
+        </div>
+        <div className="menu hidden md:block md:w-auto" id="navbar">
+          <ul className="flex p-4 md:p-0 md:flex-row md:space-x-8 mt-0">
+            {navLinks.map((link, index) => (
+              <li key={index}>
+                <NavLink href={link.path} title={link.title} />
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
       {navbarOpen ? <MenuOverlay links={navLinks} /> : null}
